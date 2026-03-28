bc8b974b971edf6d4db5f2f3d577cb853a4b8458
DineWithCelebrity <dinewithcelebrity@gmail.com>
2026-03-28 07:48:45 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,19 @@
-import { redirect } from 'next/navigation';
+import Image from "next/image";
+import styles from "./page.module.css";
+
 export default function Home() {
-  redirect('/index.html');
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <Image
+          className={styles.logo}
+          src="/next.svg"
+          alt="Next.js logo"
+          width={180}
+          height={38}
+          priority
+        />
+      </main>
+    </div>
+  );
 }
